e89c4c76267ef98d6ceb542ef1c82a4c875baa93
iTzTaisn69 <itztasinwiki@gmail.com>
2026-06-22 00:25:47 +0600

    Create itztasin69.sh

---
+++ itztasin69.sh
@@ -0,0 +1,19 @@
+apt install sudo -y
+
+# Add "add-apt-repository" command
+apt -y install software-properties-common curl apt-transport-https ca-certificates gnupg
+
+# Add additional repositories for PHP (Ubuntu 22.04)
+LC_ALL=C.UTF-8 add-apt-repository -y ppa:ondrej/php
+
+# Add Redis official APT repository
+curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
+
+# Update repositories list
+apt update
+
+# Install Dependencies
+apt -y install php8.3 php8.3-{common,cli,gd,mysql,mbstring,bcmath,xml,fpm,curl,zip} mariadb-server nginx tar unzip git redis-server
+
+curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
